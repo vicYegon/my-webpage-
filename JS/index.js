02a1b24cf0dbaf584a4a69d732605611763593ff
vicYegon <vkosgei37@gmail.com>
2023-09-21 18:34:28 +0300

    home page

--- JS/index.js
+++ JS/index.js
@@ -1,34 +1,39 @@
 const homeInfo = [
     {
         header1: "Hi Welcome", 
-        header2: "I'm Victor Yegon",
-        paragraph1: "I'm a Software Engineer based in Nairobi, kenya",
+        header2: "I'm Victor",
+        paragraph1: "I'm a Software Engineer",
+        paragraph2: "based in Nairobi Kenya",
+    },
+    {
+        header1: "Hi Welcome", 
+        header2: "I'm Yegon",
+        paragraph1: "I'm a Software Developer",
+        paragraph2: "based in Nairobi Kenya",
     },
     {
         header1: "Hi Welcome", 
         header2: "I'm Victor Yegon",
-        paragraph1: "I'm a Software Developer based in Nairobi, Kenya",
-    },
-    {
-        header1: "Hi Welcome", 
-        header2: "I'm Victor Yegon",
-        paragraph1: "I'm a Database Manager based in Nairobi kenya",
+        paragraph1: "I'm a Database Manager",
+        paragraph2: "based in Nairobi Kenya",
     }
 ]
 
 const header3El = document.querySelector("h3");
 const header2El = document.querySelector("h2");
-const paragraphEl = document.querySelector("p");
+const paragraph1El = document.querySelector(".pc1");
+const paragraph2El = document.querySelector(".pc2");
 
 let idx = 0;
 
 updateHomeInfo ();
 
 function updateHomeInfo(){
-    const {header1, header2, paragraph1} = homeInfo[idx];
+    const {header1, header2, paragraph1, paragraph2} = homeInfo[idx];
     header3El.innerText = header1;
     header2El.innerText = header2;
-    paragraphEl.innerText = paragraph1;
+    paragraph1El.innerText = paragraph1;
+    paragraph2El.innerText = paragraph2;
     idx++;
 
     if (idx === homeInfo.length){
@@ -39,28 +44,3 @@ function updateHomeInfo(){
     }, 10000)
 }
 
-
-
-// const homeBodyEl = document.querySelector(".info-container");
-// const homeInfoEl = document.querySelectorAll(".home-container");
-
-// let currentInfo = 1;
-
-// let timeOut;
-
-// updateInfo();
-
-// function updateInfo(){
-//     if(currentInfo > homeInfoEl.length){
-//         currentInfo = 1;
-//     }else if(currentInfo < 1){
-//         currentInfo = homeInfoEl.length;
-//     }
-//     homeBodyEl.style.transform = 
-//     `translateX(${(currentInfo - 1)* -600}px)`;
-
-//     timeOut = setTimeout(()=>{
-//         currentInfo++;
-//         updateInfo();
-//     }, 3000)
-// }
