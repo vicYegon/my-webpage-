f80fd06961ef4bddb9113e64a7cd8ece748aa527
vicYegon <vkosgei37@gmail.com>
2023-09-21 19:09:05 +0300

    home page

--- JS/index.js
+++ JS/index.js
@@ -3,25 +3,25 @@ const homeInfo = [
         header1: "Hi Welcome", 
         header2: "I'm Victor Yegon",
         paragraph1: "I'm a Software Engineer",
-        paragraph2: "based in Nairobi Kenya",
+        paragraph2: "based in Nairobi, Kenya",
     },
     {
         header1: "Hi Welcome", 
         header2: "I'm Victor Yegon",
         paragraph1: "I'm a Software Developer",
-        paragraph2: "based in Nairobi Kenya",
+        paragraph2: "based in Nairobi, Kenya",
     },
     {
         header1: "Hi Welcome", 
         header2: "I'm Victor Yegon",
         paragraph1: "I'm a Database Manager",
-        paragraph2: "based in Nairobi Kenya",
+        paragraph2: "based in Nairobi, Kenya",
     },
     {
         header1: "Hi Welcome", 
         header2: "I'm Victor Yegon",
         paragraph1: "I'm a Software Tester",
-        paragraph2: "based in Nairobi Kenya",
+        paragraph2: "based in Nairobi, Kenya",
     }
 ]
 
