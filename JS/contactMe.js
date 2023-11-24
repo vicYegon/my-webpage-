ee5c1e01004e762c7679a1bfcbf67583c95fc567
vicYegon <vkosgei37@gmail.com>
2023-11-24 13:44:49 +0300

    contact form working

--- JS/contactMe.js
+++ JS/contactMe.js
@@ -1,27 +1,35 @@
 const nameEl = document.getElementById("name");
 const emailEl = document.getElementById("email");
 const formEl = document.getElementById("contact_form");
+const submitEl = document.getElementById("submit");
 
 function sendEmail(){
     Email.send({
-        SecureToken: "21d93d28-0a3a-4c36-8246-57f2bbf66131",
-        To: "vicyegon4@gmail.com",
+        SecureToken: "859a6903-c697-4c1b-a332-756e9e033e5b",
+        To: "vkosgei37@gmail.com",
         From: document.getElementById("email").value,
         Subject: "New message from my website",
-        Body: "Name: " + nameEl.value
-            + "<br>Email: " + emailEl.value
-            + "<br>Subject: " + document.getElementById("subject").value
-            + "<br>Message: " + document.getElementById("message").value,
+        Body: "Name: " + nameEl.value + "<br>Email: " + emailEl.value + "<br>Subject: " + document.getElementById("subject").value + "<br>Message: " + document.getElementById("message").value,
     }).then(
-        message => alert("Message sent succesfully!", message)
+        message => {
+            alert("Message sent succesfully!", message);
+            resetForm();
+        }
     ).catch(
         error => alert("Error sending email:", error)
     );
+}
+
+function resetForm() {
+    // Reset form fields
+    nameEl.value = "";
+    emailEl.value = "";
+    document.getElementById("subject").value = "";
+    document.getElementById("message").value = "";
 }
 
 formEl.addEventListener("submit", (e)=>{
     e.preventDefault();
 
     sendEmail();
-    // console.log("mail sent");
 });
